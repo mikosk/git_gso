031fcd74f47305f6da94b38bb523cb781544b49e
mikosk <mikolajch229@nemi.tskoli.is>
2017-01-27 13:29:48 +0000

    sma prufur

--- git_gs0ö.py
+++ git_gs0ö.py
@@ -8,17 +8,18 @@ file.write("Hola evado una servesa por favor?")
 
 file.close()
 
-file = open("text.txt" , "r")
 
-file.write("ég elska pizzu")
-file.write("ég hata vakna snemma í skólann")
-file.write("gsö er skemmtilegt")
 
-file.close()
+file = open("gsogit.txt", "w")
 
-file = open("text.txt" , "r")
-
-print(file.readline())
+file.write("ég elska pizzu. ")
+file.write("ég hata vakna snemma í skólann. ")
+file.write("gsö er skemmtilegt. ")
 
 file.close()
 
+file = open("gsogit.txt", "r")
+
+print (file.read())
+
+file.close()
