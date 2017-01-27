0ab466222205b32a4f7feaa93a31e647243715bf
mikosk <mikolajch229@nemi.tskoli.is>
2017-01-27 13:24:14 +0000

    auka setningar

--- git_gs0ö.py
+++ git_gs0ö.py
@@ -7,3 +7,11 @@ file = open("gsogit.txt","w")
 file.write("Hola evado una servesa por favor?")
 
 file.close()
+
+file = open("text.txt" , "r")
+
+file.write("ég elska pizzu")
+file.write("ég hata vakna snemma í skólann")
+file.write("gsö er skemmtilegt")
+
+file.close()
